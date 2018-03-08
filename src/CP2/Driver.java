517f87e96e856259f0f0d5ffbb6eeabd1d331cb1
Moises <Moises@DESKTOP-C0EOPQP.home>
2018-03-08 12:24:36 -0500

    renamed TheLab to CP2 (is more appropriate) and added homework assignment (ExtraLab2)

---
+++ src/CP2/Driver.java
@@ -0,0 +1,38 @@
+package CP2;
+
+public class Driver {
+
+    private String name;
+    private int gallonsOfGas;
+    
+    /** @return name */
+    public String getName() {
+        return name;
+    }
+
+    /** @param name the name to set */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /** @return the gallonsOfGas */
+    public int getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    /** @param gallonsOfGas the gallonsOfGas to set */
+    public void setGallonsOfGas(int gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    public void promptUser(){
+        
+    }
+    
+    
+}
