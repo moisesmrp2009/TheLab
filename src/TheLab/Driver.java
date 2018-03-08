84784745c9fb90d9246c6627699ab1e6eef4c11e
Moises Rivas <Moises Rivas@192.168.1.9>
2018-03-08 09:55:12 -0600

    change the comments so it can be more readable

--- src/TheLab/Driver.java
+++ src/TheLab/Driver.java
@@ -5,30 +5,22 @@ public class Driver {
     private String name;
     private int gallonsOfGas;
     
-    /**
-     * @return the name
-     */
+    /** @return name */
     public String getName() {
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
+    /** @param name the name to set */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the gallonsOfGas
-     */
+    /** @return the gallonsOfGas */
     public int getGallonsOfGas() {
         return gallonsOfGas;
     }
 
-    /**
-     * @param gallonsOfGas the gallonsOfGas to set
-     */
+    /** @param gallonsOfGas the gallonsOfGas to set */
     public void setGallonsOfGas(int gallonsOfGas) {
         this.gallonsOfGas = gallonsOfGas;
     }
