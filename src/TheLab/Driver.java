48f6e6663317905cf0206bbe9fb1725be0cd03f4
Moises <Moises@DESKTOP-C0EOPQP.home>
2018-03-08 10:26:19 -0500

    new fields (name, gallonsOfGas) added + getter and setters

--- src/TheLab/Driver.java
+++ src/TheLab/Driver.java
@@ -1,6 +1,40 @@
 package TheLab;
 
 public class Driver {
+
+    private String name;
+    private int gallonsOfGas;
+    
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the gallonsOfGas
+     */
+    public int getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    /**
+     * @param gallonsOfGas the gallonsOfGas to set
+     */
+    public void setGallonsOfGas(int gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+    
+    
+    
     public static void main(String[] args) {
         
     }
